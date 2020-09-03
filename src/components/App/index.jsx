cd47a7b394a9ac5affa224f8fc62ec2ad3074a26
clickf5 <paninhome@gmail.com>
2020-09-03 16:17:34 +0300

    app in components

---
+++ src/components/App/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../Header';
+import Navbar from '../Navbar';
+import Profile from '../Profile';
+import Footer from '../Footer';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <Profile />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
